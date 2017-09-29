5243fc611d2041a812219be14bde14d00d57ca21
Julian Todd <julian@goatchurch.org.uk>
2017-09-29 16:05:17 +0100

    initial demo with parse3ddmp examples

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypothole',
+      version='0.1.0',
+      description='Various cave survey processing utilities.',
+      author='Julian Todd, Tony DiCola',
+      author_email='julian@goatchurch.org.uk',
+      url='https://github.com/goatchurchprime/pypothole',
+      packages=['pypothole'],
+      install_requires=[]
+     )
